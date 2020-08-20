dd22843dd1c9de2169e045e887aff225050d4a8c
kunnapool <35558487+kunnapool@users.noreply.github.com>
2020-08-19 18:23:32 -0700

    Additional functionality; update VM ip

--- SyncingEditor.js
+++ SyncingEditor.js
@@ -4,7 +4,8 @@ import { Slate, Editable, withReact } from 'slate-react'
 import io from 'socket.io-client'
 import { v4 as uuidv4 } from 'uuid';
 
-const socket = io.connect('http://localhost:4000')
+// GoogleVM server
+const socket = io.connect('https://34.121.79.252:4000')
 const myUUID = uuidv4();
 
 var LAMPORT_IDX = 0
@@ -24,12 +25,23 @@ function getLamportIdx() {
     return String(LAMPORT_IDX) + myUUID
 }
 
+function del_idx(idx) {
+    for(let i=0; i<ALL_DATA.length; i++) {
+        ts = ALL_DATA[i][ID_IDX]
+
+        if (ts === idx) {
+            ALL_DATA[i][ISDEL_IDX] = true
+            break
+        }
+    }
+}
+
 function init() {
 
     var id = String(0) + myUUID
 
     // val, idx, after, isDel
-    ALL_DATA.splice(0, 0, ['', 'X', null, true])
+    ALL_DATA.splice(0, 0, ['', 'X', null, false])
 }
 
 function arrToString(arr) {
@@ -48,7 +60,6 @@ function arrToString(arr) {
 }
 
 function updateXY (key) {
-    // inc can be negative
     if (key === 'ArrowLeft') EditorCurrPosX--
     else if (key === 'ArrowRight') EditorCurrPosX++
     else if (key === 'ArrowUp') EditorCurrPosY--
@@ -56,6 +67,31 @@ function updateXY (key) {
     else return false
     
     return true
+}
+
+function line_diff(last, curr) {
+    if (last.length < curr.length) {
+        for (let idx = 0; idx<last.length; idx++) {
+            if (curr[idx] != last[idx]) {
+                return curr[idx], false
+            }
+        }
+        return curr[curr.length-1], false
+    }
+    else if (last.length > curr.length) {
+        for (let idx = 0; idx<curr.length; idx++) {
+            if (curr[idx] != last[idx]) {
+                return last[idx], false
+            }
+        }
+        return curr[last.length-1], false
+    }
+    else return "", null
+
+}
+
+function buffer_send() {
+    
 }
 
 function insert (key){
@@ -73,34 +109,48 @@ function insert (key){
         var id = ALL_DATA[idx][ID_IDX]
         var isDel = ALL_DATA[idx][ISDEL_IDX]
 
+        // we are in array where we are in UI
         if (x === EditorCurrPosX -1 && y === EditorCurrPosY -1) {
             var after_idx
 
+            // predecessor for the very first element
             if (idx === 0) {
                 after_idx = 'X'
+                // idx = 1
             }
             else {
                 after_idx = ALL_DATA[idx -1][ID_IDX]
             }
 
-            LAMPORT_IDX++
             var ele = [key, getLamportIdx(), after_idx, false]
+            LAMPORT_IDX++
+
+            // insert
             ALL_DATA.splice(idx, 0, ele)
 
             break
         }
-
-        if (val === '\n') {
+        else if (val === '\n') {
             x = 0
             y++
         }
         // char is not deleted, i.e., is displayed on screen
-        if (!isDel) {
+        else if (!isDel) {
             x++
         }
-
     }
 
+}
+
+function updateEditorString(str) {
+    return (
+        [
+            {
+              type: 'paragraph',
+              children: [{ text: str}],
+            },
+        ]
+    )
 }
 
 export const SyncingEditor = () => {
@@ -112,6 +162,8 @@ export const SyncingEditor = () => {
 
         socket.on('message', msg => {
             if (msg.uuid !== myUUID) {
+                console.log('Received ', msg.msg)
+                ALL_DATA = msg.msg
             }
         });
     }, [ALL_DATA]);
@@ -125,14 +177,15 @@ export const SyncingEditor = () => {
         },
     ])
 
-    console.log(ALL_DATA)
-
     return (
-        <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+        <Slate editor={editor} value={value} onChange={newValue => {
+            // setValue(newValue)
+            
+            setValue(updateEditorString(arrToString(ALL_DATA)))
+        } }>
             <Editable
 
             onKeyDown={ event => {
-                
                 // update where we are on editor
                 if (!updateXY(event.key))
                     insert(event.key)
@@ -141,9 +194,10 @@ export const SyncingEditor = () => {
                 // notify peers of change
                 var msg = {
                     uuid: myUUID,
-                    msg: event.key
+                    msg: arrToString(ALL_DATA)
                 };
                 socket.emit('message', msg)
+                console.log('emitted')
             }}
             />
         </Slate>
